d4ed7b768b6a758c8c32e312df39fc131bb1657d
Franco.vitelli <franco.vitelli@triboo.it>
2021-07-07 15:11:58 +0200

    Added api.php and created index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require "vendor/autoload.php";
+
+use Src\DatabaseManager\DatabaseConnector;
+use Src\DatabaseManager\SurvivorConnection;
+use Src\DatabaseManager\InventoryConnection;
+use Src\DatabaseManager\TradePointsConnection;
+use Src\EntityDbHelper\DbHelper\SurvivorHelper;
+use Src\ApiController\ApiController;
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = explode( '/', $uri );
+
+if ($uri[1] !== 'api.php') {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+$envs = parse_ini_file('.env');
+foreach ($envs as $key => $value) {
+    putenv($key . '=' . $value);
+}
+
+$db = new DatabaseConnector;
+$dbConnection = $db->getConnection();
+
+$requestMethod  = $_SERVER["REQUEST_METHOD"];
+$apiController = new ApiController($requestMethod, $uri);
