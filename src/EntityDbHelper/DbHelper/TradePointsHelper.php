7c57bac6b175ebabb79765398a53cb122f84cd5f
Franco.vitelli <franco.vitelli@triboo.it>
2021-07-08 11:44:01 +0200

    Added getItemPoints method

--- src/EntityDbHelper/DbHelper/TradePointsHelper.php
+++ src/EntityDbHelper/DbHelper/TradePointsHelper.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace Src\EntityDbHelper\DbHelper;
 
 use Src\EntityDbHelper\DbHelper;
 use Src\DatabaseManager\TradePointsConnection;
-
 
 class TradePointsHelper extends DbHelper
 {
@@ -24,5 +24,22 @@ class TradePointsHelper extends DbHelper
     public function getAllTradePoints()
     {
         return $this->tradePointsConnection->selectAll();
+    }
+
+    public function getItemPoints($pointsTable)
+    {
+        $itemPoints = [];
+        foreach ($pointsTable as $value) {
+            $item = '';
+            foreach ($value as $key => $val){
+                if($key == 'item'){
+                    $item = $val ;
+                }
+                if($key == 'points'){
+                    $itemPoints[$item] = $val;
+                }
+            }
+        }
+        return $itemPoints;
     }
 }
